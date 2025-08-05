3daa64a1c65d9355233a740156ba62a9f5dbb7d8
mustazir <billah15-5193@diu.edu.bd>
2025-08-05 20:05:34 +0600

    Service Layer

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,28 +1,33 @@
 import { Request, Response } from "express";
-import { User } from "./user.model";
-import httpStatus from "http-status-codes"
 
-const createUser =async (req:Request,res:Response)=>{
+import httpStatus from "http-status-codes";
+import { UserServices } from "./user.services";
 
-    try {
-        const {name ,email }= req.body
-
-    const user =await User.create({name,email})
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const user = await UserServices.createUser(req.body);
 
     res.status(httpStatus.CREATED).json({
-        message: "user created successfully",
-        user
+      message: "user created successfully",
+      user,
     });
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-            message: "Internal server error",
-            error: error instanceof Error ? error.message : "Unknown error"
-        });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: "Internal server error",
+      error: error instanceof Error ? error.message : "Unknown error",
+    });
+  }
+};
 
-    }
-
-}
+export const UserController = { createUser };
 
 
-export const UserController = {createUser}
+
+
+
+// -----Working Flow-----
+// routes matching-> controller->services->model->DB
+
+
+// but first need to create model->create service->controller->route
