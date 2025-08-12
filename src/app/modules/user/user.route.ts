f317dd9bd26fdc70b253d4a53c9b74faf1d185a6
mustazir <billah15-5193@diu.edu.bd>
2025-08-12 20:33:31 +0600

    Complet4ed part 1

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,7 @@
 import {  Router } from "express";
 import { UserController } from "./user.controller";
 import { validateRequest } from "../../middleWires/validateRequest";
-import { createUserZodSchema } from "./user.validate";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validate";
 import { checkAuth } from "../../middleWires/checkAuth";
 import { Role } from "./user.interface";
 
@@ -10,12 +10,8 @@ import { Role } from "./user.interface";
 const router = Router();
 
 
-router.post(
-  "/register",
-  validateRequest(createUserZodSchema),
-  UserController.createUser
-);
+router.post("/register",validateRequest(createUserZodSchema),UserController.createUser);
 router.get("/all-users",checkAuth(Role.ADMIN,Role.SUPER_ADMIN),UserController.getAllUsers);
-router.patch("/:id",checkAuth(...Object.values(Role)),UserController.updateUser)
+router.patch("/:id",validateRequest(updateUserZodSchema),checkAuth(...Object.values(Role)),UserController.updateUser)
 
 export const userRoutes = router;
