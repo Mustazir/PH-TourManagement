6d7829d4e858a0430e473d23161e260aa1751232
Mustazir <billah15-5193@diu.edu.bd>
2025-09-20 22:55:15 +0600

    Seperate all error

--- src/app/middleWires/globalErrorHandler.ts
+++ src/app/middleWires/globalErrorHandler.ts
@@ -3,57 +3,62 @@
 import { NextFunction, Request, Response } from "express";
 
 import { envVars } from "../config/env";
-import { stat } from "fs";
-import { object } from "zod";
-import path from "path";
-
-export const globalErrorHandler = (err:any,req: Request,res: Response,next: NextFunction) => {
-    let statusCode=500;
-    let message=` ${err.message}`;
-    const errorSources:any=[];
-
-
-    // Duplicate user error
-    if(err.code===11000){
-        const matchedArray=err.message.match(/"([^"]*)"/);
-        statusCode=400;
-        message=`${matchedArray[1]} already exists`;
-    }
-    // Cast error/ObjectId error
-    else if(err.name==="CastError"){
-        statusCode=400;
-        message="invalid Mongoose ObjectId.Please provide a valid id";
-    }
-    // Zod validation error
-    else if(err.name==="ZodError"){
-        statusCode=400;
-        message="ZOD Validation error";
-        err.issues.forEach((issue:any)=>{
-            errorSources.push({
-                path:issue.path[issue.path.length-1],
-                message:issue.message,
-            })
-        })
-    }
+import { handleDuplicateError } from "../helpers/handleDuplicateError";
+import { handleCastError } from "../helpers/handleCastError";
+import { handleZodError } from "../helpers/handleZodError";
+import { handleValidationError } from "../helpers/handleValidationError";
+import { TErrorSources } from "../interface/error.types";
 
 
 
-    // Mongoose validation error
-    else if(err.name==="ValidationError"){
-        statusCode=400;
-        const errors= Object.values(err.errors)
-        errors.forEach((errorObject:any)=>errorSources.push({
-            path:errorObject.path,
-            message:errorObject.message,
-        }))
-        message="Validation error";
-    }
 
-    res.status(statusCode).json({
-        success: false,
-        message,
-        errorSources,
-        err,
-        stack:envVars.NODE_ENV === "development" ? err.stack : null,
-    })
+
+export const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = 500;
+  let message = ` ${err.message}`;
+  let errorSources: TErrorSources[] = [];
+
+  // Duplicate user error
+  if (err.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  // Cast error/ObjectId error
+  else if (err.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  // Zod validation error
+  else if (err.name === "ZodError") {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+  }
+
+  // Mongoose validation error
+  else if (err.name === "ValidationError") {
+
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+    message = simplifiedError.message;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    err: envVars.NODE_ENV === "development" ? err : null,
+    stack: envVars.NODE_ENV === "development" ? err.stack : null,
+  });
 };
